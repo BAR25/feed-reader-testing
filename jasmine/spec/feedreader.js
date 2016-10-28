c6bfd8df60c7b921f11ae0eed6b0842b881bdb43
Ksan8 <keziawineberg@Kezia-Winebergs-MacBook-Pro.local>
2016-10-28 00:18:58 -0700

    Modify last test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,13 +4,13 @@
 */
 
 /* All tests are placed within the $() function, since some may require DOM
-* elements; this ensures that they don"t run until the DOM is ready.
+* elements; this ensures that they don't run until the DOM is ready.
 */
 $(function() {
 
-  describe("RSS Feeds", function() {
+  describe('RSS Feeds', function() {
     /* Make sure the allFeeds variable is defined */
-    it("are defined", function() {
+    it('are defined', function() {
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
@@ -18,74 +18,80 @@ $(function() {
     /* Loop through each feed in the allFeeds object and ensure it has an URL
     * defined and that the URL is not empty.
     */
-    it("each have an url", function() {
+    it('each have an url', function() {
       var len = allFeeds.length;
       for (var i = 0; i < len; i++) {
         expect(allFeeds[i].url).toBeDefined();
-        expect(allFeeds[i].url).not.toBe("");
+        expect(allFeeds[i].url).not.toBe('');
       }
     });
 
     /* Loop through each feed in the allFeeds object and ensure it has a name
     * defined and that the name is not empty.
     */
-    it("each have an name", function() {
+    it('each have an name', function() {
       var len = allFeeds.length;
       for (var i = 0; i < len; i++) {
         expect(allFeeds[i].name).toBeDefined();
-        expect(allFeeds[i].name).not.toBe("");
+        expect(allFeeds[i].name).not.toBe('');
       }
     });
   });
 
-  describe("The menu", function() {
-    var style = $("body").hasClass("menu-hidden");
-    var menuIcon = $(".menu-icon-link");
+  describe('The menu', function() {
+    var style = $('body').hasClass('menu-hidden');
+    var menuIcon = $('.menu-icon-link');
 
     /* Ensure that the menu element is hidden by default. */
-    it("is hidden by default", function() {
+    it('is hidden by default', function() {
       expect(style).toBeTruthy();
     });
 
     /* Ensure that the menu changes visibility when the menu icon is clicked. */
-    it("becomes visible when menu icon is clicked", function() {
+    it('becomes visible when menu icon is clicked', function() {
       menuIcon.click();
-      var clickedOnce = $("body").hasClass("menu-hidden");
+      var clickedOnce = $('body').hasClass('menu-hidden');
       expect(clickedOnce).toBeFalsy();
       menuIcon.click();
-      var clickedTwice = $("body").hasClass("menu-hidden");
+      var clickedTwice = $('body').hasClass('menu-hidden');
       expect(clickedTwice).toBeTruthy();
     });
   });
 
-  describe("Initial Entries", function() {
+  describe('Initial Entries', function() {
     /* Ensure that when the loadFeed function is called and completes its work,
     * there is at least a single .entry element within the .feed container.
     */
+    var entryList;
+
     beforeEach(function(done) {
       loadFeed(0, done);
     });
 
-    it("has at least one entry", function() {
-      var entryList = $(".feed .entry");
-      expect(entryList.length > 0).toBe(true);
+    it('has at least one entry', function(done) {
+      var entryList = $('.feed .entry');
+      expect(entryList.length).toBeGreaterThan(0);
+      done();
     });
   });
 
-  describe("New Feed Selection", function() {
+  describe('New Feed Selection', function() {
     /* Ensure that when a new feed is loaded by the loadFeed function, the
     * content actually changes.
     */
     var initialFeed, newFeed;
 
     beforeEach(function(done) {
-      loadFeed(0, done);
-      initialFeed = $(".feed .entry");
-      $(".feed-list").click();
-      newFeed = $(".feed .entry");
+      loadFeed(0, function() {
+        initialFeed = $('.feed .entry');
+        loadFeed(1, function() {
+          done();
+        });
+        newFeed = $('.feed .entry');
+      });
     });
 
-    it("changes the content", function() {
+    it('changes the content', function() {
       expect(initialFeed).not.toBe(newFeed);
     });
   });
